08ec60eb6c94c5283a84b7f655c72c443015a06d
VictorGianelli <victorgnll@gmail.com>
2023-10-10 10:52:04 -0300

    first commit

---
+++ src/main/java/br/com/victorgianelli/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.victorgianelli.todolist.user;
+
+public class UserModel {
+    
+    public String userName;
+    public String name;
+    public String password;
+}
---
+++ src/test/java/br/com/victorgianelli/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorgianelli.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
